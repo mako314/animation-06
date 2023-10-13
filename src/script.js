573b33a5eb0ab5e6db14745e9588c31f577e3adf
Macolister <bispo.mac@gmail.com>
2023-10-12 20:40:54 -0400

    request animation infinite tick

--- src/script.js
+++ src/script.js
@@ -29,3 +29,13 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
+// Animations
+const tick = () => {
+    console.log('tick')
+
+    // PASS the function, do not call it.
+    // window.requestAnimationFrame(tick)
+}
+
+tick()
